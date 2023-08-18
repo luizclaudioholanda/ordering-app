84e8eecf91e94d4814aa1414730b4ce4f582241c
Luiz Claudio <luiz.holanda06@gmail.com>
2023-08-18 09:20:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Description
+
+Simple ordering app using microservices and RabbitMQ
+
+## Installation
+
+```bash
+$ yarn install
+```
+
+## Running the app
+
+```bash
+# development
+$ yarn run start
+
+# watch mode
+$ yarn run start:dev
+
+```
+
+## Running with docker
+
+```bash
+$ docker compose up --build -V
+
+```
+This command will run mongodb, rabbitmq and application
---
+++ apps/billing/src/billing.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { BillingController } from './billing.controller';
+import { BillingService } from './billing.service';
+import { RmqModule } from '@app/common/rmq/rmq.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { AuthModule } from '@app/common/auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+    RmqModule,
+    AuthModule
+  ],
+  controllers: [BillingController],
+  providers: [BillingService],
+})
+export class BillingModule {}
